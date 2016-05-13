d5da5652aa0db0d62ebd525756394a0d5a649e49
Magnus Helmersson <magnus@feomedia.com>
2016-05-14 00:20:12 +0200

    Add react-hot-loader

---
+++ react/src/server.js
@@ -0,0 +1,17 @@
+var WebpackDevServer = require("webpack-dev-server");
+var webpack = require("webpack");
+
+var config = require("./webpack.config.js");
+var compiler = webpack(config);
+
+var server = new WebpackDevServer(compiler, {
+  proxy: {
+    '/((api(-token-auth)?)|static|media|admin)/*': {
+      target: 'http://nginx'
+    }
+  },
+  publicPath: config.output.publicPath,
+  hot: true,
+  historyApiFallback: true
+});
+server.listen(8000);
--- react/src/webpack.config.js
+++ react/src/webpack.config.js
@@ -1,9 +1,20 @@
+var webpack = require("webpack");
 var HtmlWebpackPlugin = require('html-webpack-plugin');
+var combineLoaders = require('webpack-combine-loaders')
+var path = require('path');
+
 
 var webpackConfig = {
-  entry: ['babel-polyfill', './app/index.jsx'],
+  entry: {
+    app: [
+      'webpack-dev-server/client?http://localhost:8000',
+      'webpack/hot/only-dev-server',
+      'babel-polyfill',
+      './app/index.jsx'
+    ]
+  },
   output: {
-    path: 'dist',
+    path: path.resolve(__dirname, 'dist'),
     filename: 'js/bundle.js',
     publicPath: '/'
   },
@@ -14,19 +25,33 @@ var webpackConfig = {
       {
         test: /.jsx?$/,
         exclude: /(node_modules|bower_components)/,
-        loader: 'babel-loader',
-        query: {
-          presets: ['es2015', 'react'],
-          plugins: ['transform-object-rest-spread']
-        }
+        loader: combineLoaders([
+            {
+              loader: 'react-hot',
+              include: path.join(__dirname, 'app'),
+              exclude: /node_modules/
+            },
+            {
+              loader:  'babel-loader',
+              include: path.join(__dirname, 'app'),
+              exclude: /node_modules/,
+              query: {
+                presets: ['es2015', 'react'],
+                plugins: ['transform-object-rest-spread']
+              }
+            }
+        ])
       }
     ]
   },
-  plugins: [new HtmlWebpackPlugin({
-    title: 'Custom template',
-    template: './app/index.html',
-    inect: 'body'
-  })]
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Custom template',
+      template: './app/index.html',
+      inect: 'body'
+    }),
+    new webpack.HotModuleReplacementPlugin()
+  ]
 };
 
 module.exports = webpackConfig;
