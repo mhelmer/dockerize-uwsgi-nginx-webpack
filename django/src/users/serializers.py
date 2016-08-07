9366d04df0b2d11cc003e73904b9951d49b0a1bd
Magnus Helmersson <magnus@feomedia.com>
2016-08-07 17:21:05 +0200

    Move add-task from view into serializer

--- django/src/users/serializers.py
+++ django/src/users/serializers.py
@@ -1,8 +1,30 @@
 from django.contrib.auth.models import User
 from rest_framework import serializers
 
+from users.tasks import add
+
+
+class AddTask:
+    def __init__(self, a, b):
+        task = add.delay(a, b)
+        self.id = task.id
+
 
 class UserSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = User
         fields = ('url', 'username', 'email')
+
+
+class AddTaskSerializer(serializers.Serializer):
+    id = serializers.CharField(label='ID', read_only=True)
+    url = serializers.HyperlinkedIdentityField(
+        view_name='add-detail',
+        lookup_field='id',
+        lookup_url_kwarg='pk',
+    )
+    a = serializers.IntegerField(write_only=True)
+    b = serializers.IntegerField(write_only=True)
+
+    def create(self, validated_data):
+        return AddTask(a=validated_data['a'], b=validated_data['b'])
--- django/src/users/views.py
+++ django/src/users/views.py
@@ -1,12 +1,11 @@
 from django.contrib.auth.models import User
-from rest_framework.reverse import reverse
+from rest_framework import status
 from rest_framework.viewsets import ReadOnlyModelViewSet, ViewSet
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
 
 from project.celery import app
-from users.serializers import UserSerializer
-from users.tasks import add
+from users.serializers import UserSerializer, AddTaskSerializer
 
 
 class UserViewSet(ReadOnlyModelViewSet):
@@ -17,19 +16,23 @@ class UserViewSet(ReadOnlyModelViewSet):
 
 class AddTaskViewSet(ViewSet):
     permission_classes = (IsAuthenticated,)
+    serializer_class = AddTaskSerializer
 
     def create(self, request, format=None):
-        task = add.delay(request.data['a'], request.data['b'])
+        serializer = AddTaskSerializer(
+            data=request.data,
+            context={'request': request}
+        )
 
-        return Response({
-            'id': task.id,
-            'url': reverse(
-                'add-detail',
-                kwargs={'pk': task.id},
-                request=request
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        else:
+            return Response(
+                serializer.errors,
+                status=status.HTTP_400_BAD_REQUEST
             )
-        })
 
     def retrieve(self, request, pk=None):
         res = app.AsyncResult(pk)
-        return Response(res.get(timeout=2))
+        return Response(res.get())
