156d140562bf7ab0a7802c8deb957ffc64d888e6
Magnus Helmersson <magnus.helmersson@gmail.com>
2016-10-27 23:48:26 +0200

    Refactor the auth reducer

--- react/src/app/reducers/auth.js
+++ react/src/app/reducers/auth.js
@@ -1,41 +1,47 @@
 import * as actionTypes from '../constants/actionTypes'
 
-export function auth (state = {
-    isFetching: false,
-    isAuthenticated: false
-  }, action) {
-  switch (action.type) {
-    case actionTypes.LOGIN_REQUEST:
-      return {
-        ...state,
-        isFetching: true,
-        isAuthenticated: false
-      }
-    case actionTypes.LOGIN_SUCCESS:
-      return {
-        ...state,
-        token: action.token,
-        payload: action.payload,
-        isFetching: false,
-        isAuthenticated: true
-      }
-    case actionTypes.LOGIN_FAILURE:
-      return {
-        ...state,
-        token: null,
-        payload: null,
-        isFetching: false,
-        isAuthenticated: false
-      }
-    case actionTypes.LOGOUT_SUCCESS:
-      return {
-        ...state,
-        token: null,
-        payload: null,
-        isFetching: false,
-        isAuthenticated: false
-      }
-    default:
-      return state
+const updateObject = (oldObject, newValues) => ({
+  ...oldObject,
+  ...newValues,
+})
+
+const createReducer = (intialState, handlers) => (state = intialState, action) => {
+  if (handlers.hasOwnProperty(action.type)) {
+    return handlers[action.type](state, action)
   }
+  return state
 }
+
+const loginRequest = (state, action) => updateObject(state, {
+  isFetching: true,
+  isAuthenticated: false,
+})
+
+const loginSucces = (state, action) => updateObject(state, {
+  token: action.token,
+  payload: action.payload,
+  isFetching: false,
+  isAuthenticated: true,
+})
+const loginFailure = (state, action) => updateObject(state, {
+  token: null,
+  payload: null,
+  isFetching: false,
+  isAuthenticated: false,
+})
+const logoutSuccess = (state, action) => updateObject(state, {
+  token: null,
+  payload: null,
+  isFetching: false,
+  isAuthenticated: false,
+})
+
+export const auth = createReducer({
+    isFetching: false,
+    isAuthenticated: false,
+}, {
+  [actionTypes.LOGIN_REQUEST]: loginRequest,
+  [actionTypes.LOGIN_SUCCESS]: loginSucces,
+  [actionTypes.LOGIN_FAILURE]: loginFailure,
+  [actionTypes.LOGOUT_SUCCESS]: logoutSuccess,
+})
