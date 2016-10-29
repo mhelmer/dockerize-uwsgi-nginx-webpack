2f5387df9ffc18497060212ac8c0c8cd1c1407cf
Magnus Helmersson <magnus.helmersson@gmail.com>
2016-10-29 18:06:44 +0200

    Add production store

--- react/src/app/store/configureStore.prod.js
+++ react/src/app/store/configureStore.prod.js
@@ -1,14 +1,20 @@
 import { createStore, applyMiddleware } from 'redux'
 import thunkMiddleware from 'redux-thunk'
+import createSagaMiddleware from 'redux-saga'
 import rootReducer from '../reducers/index'
+import rootSaga from '../sagas'
+
+const sagaMiddleware = createSagaMiddleware()
 
 export default function configureStore(initialState) {
   const store = createStore(
     rootReducer,
     initialState,
     applyMiddleware(
-      thunkMiddleware
+      thunkMiddleware,
+      sagaMiddleware
     )
   )
+  sagaMiddleware.run(rootSaga)
   return store
 }
