646c164da7f0119ded2af8b64bbbdedfcac91e05
Magnus Helmersson <magnus.helmersson@gmail.com>
2016-11-06 23:26:21 +0100

    Add django tests to travis setup

--- django/src/users/tests.py
+++ django/src/users/tests.py
@@ -3,12 +3,13 @@ from django.core.urlresolvers import reverse
 from rest_framework import status
 from rest_framework.test import APITestCase
 
+
 class AccountTests(APITestCase):
     def test_user_list(self):
         """
         Ensure that user-list contains a list of users
         """
-        self.user = User.objects.create_user(
+        user = User.objects.create_user(
             username='jacob', email='jacob@…', password='top_secret')
 
         self.client.login(username='jacob', password='top_secret')
@@ -16,8 +17,14 @@ class AccountTests(APITestCase):
         url = reverse('user-list')
         response = self.client.get(url)
 
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
         self.assertEqual(response.data, [{
-            "url": 'http://testserver' + reverse('user-detail', kwargs={'pk': 1}),
+            "id": user.pk,
+            "url": 'http://testserver' + reverse(
+                'user-detail',
+                kwargs={'pk': 1},
+            ),
             "username": "jacob",
-            "email": "jacob@…"
+            "email": "jacob@…",
         }])
