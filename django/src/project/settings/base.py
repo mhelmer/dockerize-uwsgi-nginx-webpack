f0b907f36f66354cb0b85f70cd0a8129bc8efbb3
Magnus Helmersson <magnus.helmersson@gmail.com>
2016-11-12 12:34:32 +0100

    Add django-redis

--- django/src/project/settings/base.py
+++ django/src/project/settings/base.py
@@ -95,6 +95,16 @@ DATABASES = {
     }
 }
 
+CACHES = {
+    'default': {
+        'BACKEND': 'django_redis.cache.RedisCache',
+        'LOCATION': 'redis://redis:6379/0',
+        'OPTIONS': {
+            'CLIENT_CLASS': 'django_redis.client.DefaultClient',
+        }
+    }
+}
+
 
 # Password validation
 # https://docs.djangoproject.com/en/1.9/ref/settings/#auth-password-validators
