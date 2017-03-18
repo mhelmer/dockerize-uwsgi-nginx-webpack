f6000e0c9a219c7b261a4857d79173a102e02014
Magnus Helmersson <magnus.helmersson@gmail.com>
2017-03-19 00:16:03 +0100

    Declare sub-reducers outside reducer factory

--- react/src/app/reducers/createPagination.js
+++ react/src/app/reducers/createPagination.js
@@ -1,5 +1,5 @@
 import { has } from 'lodash'
-import { combineReducers } from 'redux'
+import { compose, combineReducers } from 'redux'
 import { createReducer } from 'reducers/utils'
 
 const reduceAllKeys = reducer => (state, action) => (
@@ -8,44 +8,66 @@ const reduceAllKeys = reducer => (state, action) => (
     return acc
   }, {})
 )
+const createPredicateReducer = (initialState, predicateReducers) => {
+  return (state = initialState, action) => {
+    const index = predicateReducers.findIndex(({ predicate }) => predicate(action))
+    return index !== -1 ? predicateReducers[index].reducer(state, action) : state
+  }
+}
 
-const withActionFilter = predicate => reducer => (state = reducer(undefined, {}), action) => {
-  if (predicate(action)) {
+const withActionFilter = validActionTypes => reducer => (state = reducer(undefined, {}), action) => {
+  if (validActionTypes.indexOf(action.type) !== -1) {
     return reducer(state, action)
   }
   return state
 }
 
-const createPagination = actionTypes => reducer => {
-  const validActionTypes = Object.keys(actionTypes).map(key => actionTypes[key])
-
-  const pageCount = createReducer(0, {
-    [actionTypes.SUCCESS]: (state, action) => action.next === null ? action.filterQuery.page
-      : Math.ceil(action.count / action.response.result.length),
-  })
-  const byPage = (state = { [1]: reducer(undefined, {}) }, action) => {
-    if(has(action, 'filterQuery.page')) {
-      return { ...state, [action.filterQuery.page]: reducer(state[action.filterQuery.page], action) }
-    }
-    if(action.type === actionTypes.INVALIDATE) {
-      return reduceAllKeys(reducer)(state, action)
-    }
-    return state
+const reducePage = reducer => (state, action) => ({
+  ...state,
+  [action.filterQuery.page]: reducer(state[action.filterQuery.page], action),
+})
+const pageCountReducer = (state, action) => {
+  if(action.next === null) {
+    return action.filterQuery.page
   }
-  const count = (state = 0, action) => has(action, 'count') ? action.count : state
-
-  const pagination = combineReducers({
-    pageCount,
-    byPage,
-    count,
-  })
-
-  const getIsMatchingType = action => validActionTypes.indexOf(action.type) !== -1
-
-  return withActionFilter(getIsMatchingType)(pagination)
+  return Math.ceil(action.count / action.response.result.length)
 }
 
-export const createPaginationSelectors = (reducer) => ({
+const count = SUCCESS => createReducer(0, { [SUCCESS]: (state, action) => action.count })
+const pageCount = SUCCESS => createReducer(0, { [SUCCESS]: pageCountReducer })
+const byPage = INVALIDATE => reducer => createPredicateReducer(
+  { [1]: reducer(undefined, {}) },
+  [ {
+    predicate: action => has(action, 'filterQuery.page'),
+    reducer: reducePage(reducer),
+  }, {
+    predicate: action => action.type === INVALIDATE,
+    reducer: reduceAllKeys(reducer),
+  } ]
+)
+
+const withPagination = ({ SUCCESS, INVALIDATE }) => reducer => combineReducers({
+  count: count(SUCCESS),
+  pageCount: pageCount(SUCCESS),
+  byPage: byPage(INVALIDATE)(reducer),
+})
+
+/**
+ * Create a higher-order reducer for pagination
+ *
+ * @param {{ SUCCESS: string, INVALIDATE: string }} actionTypes action types the reducer should listen to
+ * @returns {function(reducer: function): function} Higher order reducer
+ */
+const createPagination = actionTypes => {
+  const validActionTypes = Object.keys(actionTypes).map(key => actionTypes[key])
+
+  return compose(
+    withActionFilter(validActionTypes),
+    withPagination(actionTypes)
+  )
+}
+
+export const createPaginationSelectors = reducer => ({
   getPageCount: state => state.pageCount,
   getCount: state => state.count,
   getPage: (state, page) => state.byPage[page] || reducer(undefined, {}),
